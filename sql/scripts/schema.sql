5e4b56b83557f2aec8f7b4c5e4fe12dd693c5473
sinagua-verde-1001 <nathan.x.dpcd@gmail.com>
2016-03-22 10:50:30 -0700

    html & sql examples

---
+++ sql/scripts/schema.sql
@@ -0,0 +1,5 @@
+create table persons(
+"id" integer primary key autoincrement not null,
+"name" varchar(50) not null,
+"email" varchar(100) not null
+);
---
+++ sql/scripts/values.sql
@@ -0,0 +1,2 @@
+insert into persons(id, name, email)
+values("3", "ted cruz", "ted.cruz@gmail.com");
